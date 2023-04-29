14f6c933ca5fde92f53e9cb20a3e4e4ef08fde42
Alexandr <babiyksasha21@gmail.com>
2023-04-29 19:10:13 +0300

    start of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <div className="headerLeft">
+          <img src="/img/logo.svg" alt="logo" />
+        </div>
+
+        <ul className="headerRight">
+          <li className="">
+            <img src="/img/like.svg" alt="like" />
+          </li>
+          <li className="">
+            <img src="/img/cart.svg" alt="cart" />
+            <span>89.90$</span>
+          </li>
+        </ul>
       </header>
+
+      <section className="catalog">
+        <h1>Catalog</h1>
+        <ul className="catalogList">
+          <li className="card">
+            <div className="cardInfo">
+              <span className="cardInfoText">Out of stock</span>
+              <span className="cardInfoSale">sale</span>
+            </div>
+            <img src="/img/locks/lock-1.jpg" alt="lock" />
+            <p>Variable Golden Soft lock for the hotel</p>
+            <div className="cardInner">
+              <div className="cardInnerPrice">
+                <span>89.90$</span>
+                <span>100.00$</span>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </li>
+
+          <li className="card">
+            <div className="cardInfo">
+              <span className="cardInfoText">Out of stock</span>
+              <span className="cardInfoSale">sale</span>
+            </div>
+            <img src="/img/locks/lock-2.jpg" alt="lock" />
+            <p>Variable Golden Soft lock for the hotel</p>
+            <div className="cardInner">
+              <div className="cardInnerPrice">
+                <span>89.90$</span>
+                <span>100.00$</span>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </li>
+
+          <li className="card">
+            <div className="cardInfo">
+              <span className="cardInfoText">Out of stock</span>
+              <span className="cardInfoSale">sale</span>
+            </div>
+            <img src="/img/locks/lock-3.jpg" alt="lock" />
+            <p>Variable Golden Soft lock for the hotel</p>
+            <div className="cardInner">
+              <div className="cardInnerPrice">
+                <span>89.90$</span>
+                <span>100.00$</span>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </li>
+
+          <li className="card">
+            <div className="cardInfo">
+              <span className="cardInfoText">Out of stock</span>
+              <span className="cardInfoSale">sale</span>
+            </div>
+            <img src="/img/locks/lock-4.jpg" alt="lock" />
+            <p>Variable Golden Soft lock for the hotel</p>
+            <div className="cardInner">
+              <div className="cardInnerPrice">
+                <span>89.90$</span>
+                <span>100.00$</span>
+              </div>
+              <button>Add to cart</button>
+            </div>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
